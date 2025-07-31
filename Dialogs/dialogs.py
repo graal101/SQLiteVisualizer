d51e9a2f3f81c7fbee479165e543a790e5258f1a
Andrew Andrey <graal101@mail.ru>
2025-07-31 15:53:16 +0700

    Add choose font in class

--- Dialogs/dialogs.py
+++ Dialogs/dialogs.py
@@ -13,20 +13,21 @@ def message(self, title: str, board_text: str, ico=1):
         msg.setWindowTitle(title)
         msg.setStandardButtons(QMessageBox.StandardButton.Ok)
         msg.exec()
-        
-        
+
+
 class FileDialog(QWidget):
     def __init__(self):
         super().__init__()
+        self.filter_dialog = 'All Files (*.*);;SQLite db Files (*.db);;SQLite Files (*.sqlite3)'
 
     def open_file_dialog(self):
         """Диалог открытия файла."""
         options = QFileDialog.Option.DontUseNativeDialog
         file_dialog = QFileDialog(self)
         file_dialog.setOptions(options)
-        file_dialog.setNameFilter("All Files (*.*);;SQLite Files (*.sqlite3)")
+        file_dialog.setNameFilter(self.filter_dialog)
         file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptOpen)
-        
+
         if file_dialog.exec():
             file_path = file_dialog.selectedFiles()[0]
             return file_path
@@ -37,10 +38,17 @@ class FileDialog(QWidget):
         options = QFileDialog.Option.DontUseNativeDialog
         file_dialog = QFileDialog(self)
         file_dialog.setOptions(options)
-        file_dialog.setNameFilter("All Files (*.*);;SQLite Files (*.sqlite3)")
+        file_dialog.setNameFilter(self.filter_dialog)
         file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptSave)
-        
+
         if file_dialog.exec():
             file_path = file_dialog.selectedFiles()[0]
             return file_path
+        return None
+
+    def font_dialog(self):
+        """Диалог выбора шрифта."""
+        font, ok = QFontDialog.getFont()
+        if ok:
+            return font
         return None
--- main.py
+++ main.py
@@ -17,11 +17,17 @@ class MyApp(QtWidgets.QMainWindow):
         # Здесь сигналы и слоты
         self.mn_open.triggered.connect(self.mn_open_file)  # Открыть файл БД
         self.mn_quit.triggered.connect(self.mn_exit)  # Выход
-
+        self.mn_font.triggered.connect(self.mn_font_choose)  # Выбор шрифта, размера шрифта
+        
     def mn_open_file(self):
-        #message(self, 'Открыто', 'Имя файла')
         d = FileDialog()
         print(d.open_file_dialog())
+        
+    def mn_font_choose(self):
+        d = FileDialog()
+        fsize = d.font_dialog()
+        if fsize:
+            self.menubar.setFont(fsize)  # for test
 
     def mn_exit(self):
         # Действие при нажатии кнопки
